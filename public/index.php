c720305ca1a8f16c028fe7ad3d8e9584cf4989fd
marc_tappx <marc@tappx.com>
2021-03-29 00:14:25 +0200

    UPDATE: index.php

--- public/index.php
+++ public/index.php
@@ -1,10 +1,5 @@
 <?php
 
-require_once __DIR__
-  . '/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
-use ThePHPWebsite\App;
-
-$app = new App();
-
-$app->sayHello();
+echo "<h1>Index super php stack</h1>";
