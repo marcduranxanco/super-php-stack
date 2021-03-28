e2e9574d885243ac6ffa51185cf741b81b6fe0f6
marc_tappx <marc@tappx.com>
2021-03-29 00:12:26 +0200

    UPDATE: app test and class

--- src/App.php
+++ src/App.php
@@ -2,12 +2,21 @@
 
 declare(strict_types=1);
 
-namespace ThePHPWebsite;
+namespace App;
 
 class App
 {
-  public function sayHello(): void
-  {
-    echo 'Hello!';
-  }
+
+    protected $nombre;
+
+    public function __construct($nombre)
+    {
+        $this->nombre = $nombre;
+    }
+
+    public function sayHello(): string
+    {
+        $hello = "Hello $this->nombre!";
+        return $hello;
+    }
 }
